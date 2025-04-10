cfcb3423ce3ac8eac1c5a04d966afdf5b9903ef1
itvchi <jac.pikula@gmail.com>
2025-04-10 20:29:59 +0200

    PicoRV32 - UART over BL702

--- 006_picoRV/putty.sh
+++ 006_picoRV/putty.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-GDK_BACKEND=x11 putty -serial /dev/ttyACM0 -sercfg 115200,8,n,1,n
+# Exec "sudo modprobe ftdi_sio" if BL702 is not binded to /dev/ttyUSB*
+GDK_BACKEND=x11 putty -serial /dev/ttyUSB1 -sercfg 115200,8,n,1,n
