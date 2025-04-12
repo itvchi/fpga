2a01da0608c296b661507aeda115596eaa3d37e9
itvchi <jac.pikula@gmail.com>
2025-04-12 22:00:14 +0200

    PicoRV32 - added Kbuild with menuconfig for choosing configuration

--- 006_picoRV/firmware/src/app_base.c
+++ 006_picoRV/firmware/src/app_base.c
@@ -3,17 +3,19 @@
 
 int app_base() {
 
-    char character;
-
+#if defined(CONFIG_WITH_UART)
     uart_init(F_CPU / (2 * BAUDRATE));
     uart_print_irq("Hello world!\r\n");
+#endif /* defined(CONFIG_WITH_UART) */
 
     while (1) {
+#if defined(CONFIG_WITH_UART)
         uart_print_irq("Press any key\r\n");
-        character = uart_get();
+        char character = uart_get();
         uart_print_irq("\rYou pressed: ");
         uart_put(character);
         uart_print_irq("\r\n");
+#endif /* defined(CONFIG_WITH_UART) */
     }
 
     return 0;
--- 006_picoRV/firmware/src/main.c
+++ 006_picoRV/firmware/src/main.c
@@ -12,6 +12,7 @@ void led_action(void* ctx) {
     set_leds(*led_ctx);
 }
 
+int app_minimal();
 int app_base();
 int app_lcd();
 
@@ -20,9 +21,18 @@ int main() {
     // perf_flash();
     // perf_ram();
     
+#if defined(CONFIG_WITH_SYSTICK)
     systick_add_event(led_action, (void*)&leds, SYSTICK_PRIO_HIGH, 2);
     systick_init_irq(0, F_CPU/10);
+#endif /* defined(CONFIG_WITH_SYSTICK) */
 
+#if defined(CONFIG_FS_MINIMAL)
+    return app_minimal();
+#elif defined(CONFIG_FS_BASE)
     return app_base();
-    //return app_lcd();
+#elif defined(CONFIG_FS_LCD)
+    return app_lcd();
+#else 
+    return 0;
+#endif
 }
