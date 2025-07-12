48413d1755ef85d353b7a8b9adb23086e97f2b0e
itvchi <jac.pikula@gmail.com>
2025-07-12 15:33:36 +0200

    PicoRV32 - changed uart message (linked with libc)

--- 006_picoRV/firmware/src/app_base.c
+++ 006_picoRV/firmware/src/app_base.c
@@ -1,23 +1,41 @@
 #include "system.h"
 #include "uart.h"
 #include "systick.h"
+#include <stdio.h>
+#include <stdbool.h>
 
+typedef struct {
+    int value;
+    bool updated;
+} counter_t;
 
 void uart_action(void* ctx) {
 
-    uart_print_irq("Hello RISC-V!\r\n");
+    counter_t *counter = (counter_t *)ctx;
+    counter->value++;
+    counter->updated = true;
 }
 
 void app_base() {
+
+    char buffer[64];
+    counter_t counter = {};
 
 #if defined(CONFIG_WITH_UART)
     uart_init(F_CPU / (2 * BAUDRATE));
     uart_print_irq("Hello world!\r\n");
 #endif /* defined(CONFIG_WITH_UART) */
 #if defined(CONFIG_WITH_SYSTICK)
-    systick_add_event(uart_action, 0, SYSTICK_PRIO_LOW, 10);
+    systick_add_event(uart_action, (void *)&counter, SYSTICK_PRIO_LOW, 10);
 #endif /* defined(CONFIG_WITH_SYSTICK) */
 
     while (1) {
+        if (counter.updated) {
+            counter.updated = false;
+            sprintf(buffer, "Counter is %d\r\n", counter.value);
+#if defined(CONFIG_WITH_UART)
+            uart_print_irq(buffer);
+#endif /* defined(CONFIG_WITH_UART) */
+        }
     }
 }
