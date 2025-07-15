d23720230d71bca653457265f8eee7b9d64494d7
itvchi <jac.pikula@gmail.com>
2025-07-15 23:00:59 +0200

    PicoRV32 - preparing buildsystem for new board

--- 006_picoRV/tcl/base_9k.tcl
+++ 006_picoRV/tcl/base_9k.tcl
@@ -1,14 +1,19 @@
 create_project -name 006_picoRV -pn GW1NR-LV9QN88PC6/I5 -device_version C -force -dir ../
-add_file -type verilog "src/top_minimal.v"
+add_file -type verilog "src/top_base.v"
 add_file -type verilog "src/picorv32.v"
 add_file -type verilog "src/reset_control.v"
 add_file -type verilog "src/sram.v"
 add_file -type verilog "src/gowin_user_flash/gowin_user_flash.v"
+add_file -type verilog "src/gowin_rpll/gowin_rpll.v"
 add_file -type verilog "src/user_flash_custom.v"
 add_file -type verilog "src/mm_leds.v"
-add_file -type cst "constraints/minimal.cst"
+add_file -type verilog "src/systick.v"
+add_file -type verilog "src/uart.v"
+add_file -type verilog "src/spi.v"
+add_file -type verilog "src/gpio.v"
+add_file -type cst "constraints/base_9k.cst"
 add_file -type sdc "constraints/common.sdc"
 set_option -synthesis_tool gowinsynthesis
 set_option -output_base_name picoRV
-set_option -top_module top
+set_option -top_module top_base
 run all
