7dc506f5e7277dfb9dc4224f6020664a8e3341cb
itvchi <jac.pikula@gmail.com>
2025-10-12 13:47:16 +0200

    SPI master - added single byte reception test

--- 008_spi_flash/src/spi_master_tb.v
+++ 008_spi_flash/src/spi_master_tb.v
@@ -26,7 +26,10 @@ module spi_master_tb();
     spi_master UUT (
         .clk(clk),
         .rst_n(rst_n),
-        .prescaler(8'd0),
+        .prescaler(8'd1), /* Increase prescaler, because miso driver have lag (this change not influence tested module behaviour, 
+            but allows to change testbench signals in more suitable moment - without it we have one clock cycle lag for miso signal, and another lag inside UUT, 
+            which filters metastability on spi_miso pin - another lag which enforced minimal hardware prescaler value, but this can be done at this side and 
+            will not influence module behaviour, so it will work the same if mosi signal will change at edge change, not one clk after it as in testbench) */
         .cpol(cpol),
         .cpha(cpha),
         .tx_data(tx_data),
@@ -94,6 +97,19 @@ module spi_master_tb();
         end
     endtask
 
+    task wait_rx_valid;
+        integer timeout;
+        begin
+            timeout = 0;
+            while (!rx_valid && timeout < 1000000) begin
+                @(posedge clk);
+                timeout = timeout + 1;
+            end
+            if (timeout >= 1000000)
+                $fatal(1, "Timeout waiting for rx_valid!");
+        end
+    endtask
+
     task send_byte_nowait(input [7:0] byte);
         begin
             @(posedge clk);
@@ -156,15 +172,45 @@ module spi_master_tb();
         end
     endtask
 
+    task test_receive_byte(input polarity, input phase, input [7:0] byte);
+        reg [7:0] rx_byte;
+        begin
+            test_count = test_count + 1;
+
+            cpol <= polarity;
+            cpha <= phase;
+            spi_miso_data <= byte; /* Configure data which will be put on MISO */
+            #200;
+            send_byte_nowait(8'h00);
+            @(posedge clk);
+            wait_rx_valid();
+            rx_byte = rx_data; // blocking (this is ok - or use non-blocking but wait for clock cycle to register)
+            wait_not_busy();
+
+            if (rx_byte === byte) begin
+                $display("\tReceived byte 0x%02h - OK", byte);
+            end else begin
+                error_count = error_count + 1;
+                $display("\tReceived byte 0x%02h - FAIL (expected 0x%02h)", rx_byte, byte);
+            end
+        end
+    endtask
+
     //---------------------------------------
     // SPI MOSI monitor - probes mosi signal to compare if it is same as tx_data
+    // SPI MISO driver - puts miso signal on the bus to compare with rx_data
     //---------------------------------------
     reg spi_cs_prev;
     reg spi_clk_prev;
     reg spi_mosi_prev;
     reg [7:0] spi_mosi_data;
+    reg [7:0] spi_miso_data;
     reg [7:0] spi_data;
     reg [2:0] spi_bitcount;
+
+    initial begin
+        spi_miso_data = 1'b0;
+    end
 
     always @(posedge clk) begin
         /* Register previous signals for edge detection and common probe time */
@@ -176,24 +222,42 @@ module spi_master_tb();
         if ({spi_cs_prev, spi_cs} == 2'b10) begin
             spi_mosi_data <= 8'd0;
             spi_bitcount <= 3'd0;
+            spi_miso <= spi_miso_data[7];
+            /* For cpha == 1'b1, the change should omit first change edge, otherwise we can shift here */
+            if (cpha == 1'b0) begin
+                spi_miso_data <= {spi_miso_data[6:0], spi_miso_data[7]};
+            end
         end
         /* SPI chip select is inactive */
         if ({spi_cs_prev, spi_cs} == 2'b01) begin
             spi_data <= spi_mosi_data;
+            spi_miso <= 1'b0;
         end
 
-        if (cpol == cpha) begin 
-            /* Probe mosi at rising edge */
-            if ({spi_clk_prev, spi_clk} == 2'b01) begin
-                spi_bitcount <= spi_bitcount + 3'd1;
-                spi_mosi_data <= {spi_mosi_data[6:0], spi_mosi_prev};
+        if ({spi_cs_prev, spi_cs} == 2'b00) begin
+            if (cpol == cpha) begin 
+                /* Probe mosi at rising edge */
+                if ({spi_clk_prev, spi_clk} == 2'b01) begin
+                    spi_bitcount <= spi_bitcount + 3'd1;
+                    spi_mosi_data <= {spi_mosi_data[6:0], spi_mosi_prev};
+                end
+                /* Change miso at falling edge */
+                if ({spi_clk_prev, spi_clk} == 2'b10) begin
+                    spi_miso_data <= {spi_miso_data[6:0], spi_miso_data[7]};
+                    spi_miso <= spi_miso_data[7];
+                end
+            end else begin 
+                /* Probe mosi at falling edge */
+                if ({spi_clk_prev, spi_clk} == 2'b10) begin
+                    spi_bitcount <= spi_bitcount + 3'd1;
+                    spi_mosi_data <= {spi_mosi_data[6:0], spi_mosi_prev};
+                end   
+                /* Change miso at rising edge */
+                if ({spi_clk_prev, spi_clk} == 2'b01) begin
+                    spi_miso_data <= {spi_miso_data[6:0], spi_miso_data[7]};
+                    spi_miso <= spi_miso_data[7];
+                end          
             end
-        end else begin 
-            /* Probe mosi at falling edge */
-            if ({spi_clk_prev, spi_clk} == 2'b10) begin
-                spi_bitcount <= spi_bitcount + 3'd1;
-                spi_mosi_data <= {spi_mosi_data[6:0], spi_mosi_prev};
-            end             
         end
 
         /* Latch mosi registered data from shift register at first spi_clk_edge when spi_bitcount wraps */
@@ -229,12 +293,24 @@ module spi_master_tb();
         // ---------- TX multi byte test ----------
         $display("=== TX multi byte test ===");
         test_send_bytes(8'h22, 8'hC3, 8'h44, 8'hD2);
+        #100
+
+        // ---------- RX single byte test ----------
+        $display("=== RX single byte test ===");
+        test_receive_byte(1'b0, 1'b0, 8'hA5);
+        test_receive_byte(1'b1, 1'b0, 8'hA5);
+        test_receive_byte(1'b0, 1'b1, 8'hA5);
+        test_receive_byte(1'b1, 1'b1, 8'hA5);
+        #100;
+        cpol = 0;
+        cpha = 0;
+        #100;
 
         // ---------- Summarize test results ----------
         if (error_count) begin
-            $display("TEST FAILED - (%0d/%0d)", error_count, test_count);
+            $display("TEST FAILED - (%0d/%0d OK)", test_count - error_count, test_count);
         end else begin
-            $display("TEST PASSED - (%0d/%0d)", test_count, test_count);
+            $display("TEST PASSED - (%0d/%0d OK)", test_count, test_count);
         end
         #200;
         $finish;
