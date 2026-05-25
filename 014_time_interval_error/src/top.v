0e1b2de35111b360c2fafb02d43c3f67dc02e542
itvchi <jac.pikula@gmail.com>
2026-05-25 06:52:05 +0200

    Time interval error - increased clock frequency and checked results

--- 014_time_interval_error/src/top.v
+++ 014_time_interval_error/src/top.v
@@ -6,10 +6,19 @@ module top (
     output pps_meas3
 );
 
+    wire pll_clk_fast; //81 MHz
+    wire pll_clk_slow; //20.25 MHz
+
+    Gowin_rPLL pll(
+        .clkout(pll_clk_fast),
+        .clkoutd(pll_clk_slow),
+        .clkin(clk)
+    );
+
     wire rst_n;
 
     reset global_reset (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .rst_n(rst_n)
     );
 
@@ -17,18 +26,18 @@ module top (
     wire ppsA;
 
     counter #(
-        .VALUE(27000000)
+        .VALUE(81000000)
     ) reference (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .clk_en(1'b1),
         .rst_n(rst_n),
         .pps(ppsA)
     );
 
     pulse_stretching #(
-        .CLK(27000000)
+        .CLK(81000000)
     ) ps_ref (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .pulse_in(ppsA),
         .pulse_out(pps_ref)
     );
@@ -37,18 +46,18 @@ module top (
     wire ppsB;
 
     counter #(
-        .VALUE(27000001)
+        .VALUE(81000001)
     ) slower (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .clk_en(1'b1),
         .rst_n(rst_n),
-        .pps(ppsB) //26 999 999.000 000 04 Hz (-0.999 999 06 Hz = -37 ppb)
+        .pps(ppsB) //80 999 999.000 000 01 Hz (-0.999 999 09 Hz = -12.3 ppb)
     );
 
     pulse_stretching #(
-        .CLK(27000000)
+        .CLK(81000000)
     ) ps_meas1 (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .pulse_in(ppsB),
         .pulse_out(pps_meas1)
     );
@@ -58,9 +67,9 @@ module top (
     wire ppsC;
 
     acc_counter #(
-        .VALUE(1_590_728_628) //9 999 999.999 068 Hz (-0.000 932 Hz = −0.093 ppb)
+        .VALUE(530_242_876) //9 999 999.999 069 Hz (-0.000 931 = -0.093 ppb)
     ) acc_counter_10_slow (
-        .clk_ref(clk),
+        .clk_ref(pll_clk_fast),
         .rst_n(rst_n),
         .clk_en(clk_en_10_slow)
     );
@@ -68,16 +77,16 @@ module top (
     counter #(
         .VALUE(10000000)
     ) counter_10_slow (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .clk_en(clk_en_10_slow),
         .rst_n(rst_n),
         .pps(ppsC)
     );
 
     pulse_stretching #(
-        .CLK(27000000)
+        .CLK(81000000)
     ) ps_meas2 (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .pulse_in(ppsC),
         .pulse_out(pps_meas2)
     );
@@ -87,9 +96,9 @@ module top (
     wire ppsD;
 
     acc_counter #(
-        .VALUE(1_590_728_629) // 10 000 000.005 355 Hz (+0.005 355 Hz = 0.5355 ppb)
+        .VALUE(530_242_877) // 10 000 000.018 Hz (+0.018 Hz = 1.8ppb)
     ) acc_counter_10_fast (
-        .clk_ref(clk),
+        .clk_ref(pll_clk_fast),
         .rst_n(rst_n),
         .clk_en(clk_en_10_fast)
     );
@@ -97,16 +106,16 @@ module top (
     counter #(
         .VALUE(10000000)
     ) counter_10_fast (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .clk_en(clk_en_10_fast),
         .rst_n(rst_n),
         .pps(ppsD)
     );
 
     pulse_stretching #(
-        .CLK(27000000)
+        .CLK(81000000)
     ) ps_meas3 (
-        .clk(clk),
+        .clk(pll_clk_fast),
         .pulse_in(ppsD),
         .pulse_out(pps_meas3)
     );
@@ -115,10 +124,10 @@ endmodule
 
 /* Results
     PPS drift after 15min: 
-    pps_meas1: 34us (37.8 ppb)
-    pps_meas2: 150ns (0.17 ppb)
-    pps_meas3: 480ns (0.54 ppb)
+    pps_meas1: 44.6us (12.4 ppb) 
+    pps_meas2: 150ns (0.1 ppb)
+    pps_meas3: 6.5us (1.8 ppb)
 
     Meas results (specially for pps_meas2 from clock with least error) are slightly of calculated values
-    because pulse_stretching resulution is 37 ns and measurement was to short to achive good resolution
+    because pulse_stretching resulution is 12.3 ns and measurement was to short to achive good resolution
 */
